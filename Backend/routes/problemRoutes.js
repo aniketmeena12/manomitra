948958a6e1005a1b35ac16ee657d42c1201805f3
aniket meena <meenaaniket066@gmail.com>
2025-09-14 15:43:31 +0530

    almostdoing

--- Backend/routes/problemRoutes.js
+++ Backend/routes/problemRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { addProblem, getProblems, reactProblem } = require("../controllers/problemController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authmiddleware.js");
 
 const router = express.Router();
 
